6712ad9c94ec3e3dcedc9035a11b66c2387ef026
Saboor-Hamedi <janhamedi49@gmail.com>
2024-03-13 11:14:22 +0700

    The numberOfLines

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
+  let x = 1;
+
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView >
+      
+      <Text style = {styles.title}>
+      In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available
+      </Text>
+      <Button title='click me'></Button>
+     
+    </SafeAreaView>
+     
+
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ title:{
+ 
+  fontWeight: 'bold',
+  fontSize:40,
+  textAlign: 'center',
+  backgroundColor: 'yellow',
+  
+ }
+})  
+
